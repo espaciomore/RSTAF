57435101f906159de967006e810fef0337500f10
Manuel A. Cerda <espaciomore@gmail.com>
2016-03-25 08:38:49 -0300

    Update sample_net_ssh.rb

--- sample_net_ssh.rb
+++ sample_net_ssh.rb
@@ -2,9 +2,9 @@
  require 'rubygems'
  require 'net/ssh'
  
- HOST = '10.0.0.29'
- USER = 'noodleqa'
- PASS = 'welc0me'
+ HOST = 'INSERT HOST IP ADDRESS'
+ USER = 'INSERT HOST ACCOUNT'
+ PASS = 'INSERT HOST PASSWORD'
  
  Net::SSH.start( HOST, USER, :password => PASS ) do|ssh|
   #result = ssh.exec!('ls /var/www/html/continuous_delivery/acceptance_tests/sockets/reports')
